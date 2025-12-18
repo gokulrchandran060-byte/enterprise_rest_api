85946550472b12482820df4d2e5606a44a86e10b
gokulrchandran060-byte <gokulrchandran060@gmail.com>
2025-12-18 22:39:08 +0530

    Added DRF serializer with echo API and validation

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HealthCheckAPIView
+from .views import HealthCheckAPIView, EchoAPIView
 
 urlpatterns = [
     path('health/', HealthCheckAPIView.as_view()),
+    path('echo/', EchoAPIView.as_view()),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .serializers import EchoSerializer
 
 class HealthCheckAPIView(APIView):
     def get(self, request):
@@ -11,3 +12,20 @@ class HealthCheckAPIView(APIView):
             },
             status=status.HTTP_200_OK
         )
+
+
+class EchoAPIView(APIView):
+    def post(self, request):
+        serializer = EchoSerializer(data=request.data)
+
+        if serializer.is_valid():
+            return Response(
+                {"echo": serializer.validated_data["message"]},
+                status=status.HTTP_200_OK
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
