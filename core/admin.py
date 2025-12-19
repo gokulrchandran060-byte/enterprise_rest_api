50f14768b3cf2bedee12b43b6304a8fd2896c803
gokulrchandran060-byte <gokulrchandran060@gmail.com>
2025-12-19 13:53:38 +0530

    Registered Message model in Django admin for data inspection

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Message
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'content', 'created_at')
